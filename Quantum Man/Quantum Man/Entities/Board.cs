86670d5923fb0617d176868a8a59cf950b54e1aa
Emil Juul Jacobsen <emilovicmail@gmail.com>
2012-02-11 23:57:39 +0100

    made map scroll, needs more refactoring

--- Quantum Man/Quantum Man/Entities/Board.cs
+++ Quantum Man/Quantum Man/Entities/Board.cs
@@ -85,15 +85,6 @@ namespace Quantum_Man.Entities
             }
         }
 
-        private void Initialize(int width, int height)
-        {
-            Obstacles = new Obstacle[width, height];
-            Creatures = new Creature[width,height];
-            Layer0 = new int[width, height];
-            Layer1 = new int[width, height];
-            Layer2 = new int[width, height];
-        }
-
         public void Draw(int x, int y, Point destination, GameTime time, SpriteBatch draw)
         {
             Rectangle dest = new Rectangle(destination.X, destination.Y, TileSize, TileSize);
@@ -102,8 +93,20 @@ namespace Quantum_Man.Entities
             TileSet.Draw(Layer1[x, y], dest, draw);
 
             if(Obstacles[x,y] != null) Obstacles[x,y].Draw(dest, time,draw);
+            if(Creatures[x,y] != null) Creatures[x,y].Draw(dest,time,draw);
 
             TileSet.Draw(Layer2[x, y], dest, draw);
+        }
+
+        public void Draw(int xStart, int width, int yStart, int height, Point origin, GameTime time, SpriteBatch draw)
+        {
+            for(int y = yStart; y <= yStart + height; y++)
+            {
+                for(int x = xStart; x <= xStart + width; x++)
+                {
+                    this.Draw(x,y, new Point(origin.X + (x-xStart)*TileSize, origin.Y + (y-yStart)*TileSize),time,draw);
+                }
+            }
         }
 
         public Point GetPosition(Entity entity)
@@ -120,7 +123,67 @@ namespace Quantum_Man.Entities
             return new Point(-1,-1);
         }
 
+        public void MoveCreature(Creature c, Direction dir)
+        {
+            Point p = GetPosition(c);
+            switch(dir)
+            {
+                case Direction.Up:
+                    {
+                        if(WithinBounds(p.X,p.Y-1))
+                        {
+                            Creatures[p.X, p.Y] = null;
+                            Creatures[p.X, p.Y - 1] = c;
+                        }
+                        break;
+                    }
+                case Direction.Down:
+                    {
+                        if(WithinBounds(p.X, p.Y + 1))
+                        {
+                            Creatures[p.X, p.Y] = null;
+                            Creatures[p.X, p.Y + 1] = c; 
+                        }
+                        break;
+                    }
+                case Direction.Left:
+                    {
+                        if(WithinBounds(p.X - 1, p.Y))
+                        {
+                            Creatures[p.X, p.Y] = null;
+                            Creatures[p.X - 1, p.Y] = c;
+                        }
+                        break;
+                    }
+                case Direction.Right:
+                    {
+                        if(WithinBounds(p.X + 1, p.Y))
+                        {
+                            Creatures[p.X, p.Y] = null;
+                            Creatures[p.X + 1, p.Y] = c;
+                        }
+                        break;
+                    }
+            }
+            c.Face(dir);
+        }
+
         #region private methods
+
+        private void Initialize(int width, int height)
+        {
+            Obstacles = new Obstacle[width, height];
+            Creatures = new Creature[width, height];
+            Layer0 = new int[width, height];
+            Layer1 = new int[width, height];
+            Layer2 = new int[width, height];
+        }
+
+        private bool WithinBounds(int X, int Y)
+        {
+            if (X >= 0 && X < Width && Y >= 0 && Y < Height) return true;
+            return false;
+        }
 
         private Point Query<T>(Entity entity, T[,] array) where T : Entity
         {
@@ -128,7 +191,7 @@ namespace Quantum_Man.Entities
             {
                 for(int y = 0; y < array.GetLength(1); y++)
                 {
-                    if(array[x,y].Equals(entity)) return new Point(x,y);
+                    if(array[x,y] != null && array[x,y].Equals(entity)) return new Point(x,y);
                 }
             }
 
--- Quantum Man/Quantum Man/Screen/TileTestScreen.cs
+++ Quantum Man/Quantum Man/Screen/TileTestScreen.cs
@@ -12,53 +12,50 @@ namespace Quantum_Man.Screen
 {
     public class TileTestScreen : GameScreen
     {
+        private KeyboardState lastState;
+        private KeyboardState curState;
+
         private Board board;
         private Creature player;
 
         public TileTestScreen(ScreenManager manager) : base(manager)
         {
-            //board = new Board("level1",manager.Game.Content);
-            TileSet tileSet = new TileSet(manager.Game.Content.Load<Texture2D>("tileset1"));
-            board = new Board(tileSet,100,100);
-            //board.Obstacles[10,5] = new Obstacle(10,5,tileSet,9);
+            board = new Board("level1",manager.Game.Content);
+            //board = new Board(new TileSet(manager.Game.Content.Load<Texture2D>("tileset1")),100,100 );
+
             player = new Creature(new TileSet(manager.Game.Content.Load<Texture2D>("playersprite")));
+            board.Creatures[5, 5] = player;
         }
 
         public override void Draw(GameTime time, SpriteBatch draw)
         {
-            // print stuff
-            for (int y = 0; y < board.Height; y++ )
-            {
-                for(int x = 0; x < board.Width; x++)
-                {
-                    board.Draw(x,y,new Point(x * board.TileSize, y * board.TileSize), time, draw);
-                }
-            }
+            Point p = board.GetPosition(player);
+            int x = Math.Max(Math.Min(p.X - 5,board.Width - 11),0);
+            int y = Math.Max(Math.Min(p.Y - 5, board.Height - 11), 0);
 
-            player.Draw(new Rectangle(0,0,board.TileSize,board.TileSize), time, draw);
+            board.Draw(x,10,y,10,new Point(10,10),time,draw );
 
             base.Draw(time, draw);
         }
-
-        private bool upDown = false;
-        private bool downDown = false;
-        private bool f2Down = false;
 
         public override void Update(GameTime time)
         {
             base.Update(time);
 
-            KeyboardState ks = Keyboard.GetState();
-            if (ks.IsKeyDown(Keys.Up) && !upDown){board.TileSize += 10;}
-            if (ks.IsKeyDown(Keys.Down) && !downDown) board.TileSize -= 10;
+            curState = Keyboard.GetState();
+            if (KeyPressed(Keys.Up)) board.MoveCreature(player,Direction.Up);
+            if (KeyPressed(Keys.Down)) board.MoveCreature(player, Direction.Down);
+            if (KeyPressed(Keys.Right)) board.MoveCreature(player, Direction.Right);
+            if (KeyPressed(Keys.Left)) board.MoveCreature(player, Direction.Left);
 
-            if (ks.IsKeyDown(Keys.F2) && !f2Down) ((Game1)this.ScreenManager.Game).graphics.ToggleFullScreen();
+            if (KeyPressed(Keys.F2)) ((Game1)this.ScreenManager.Game).graphics.ToggleFullScreen();
 
-            upDown = ks.IsKeyDown(Keys.Up);
-            downDown = ks.IsKeyDown(Keys.Down);
-            f2Down = ks.IsKeyDown(Keys.F2);
+            lastState = curState;
+        }
 
-            player.Update(time);
+        public bool KeyPressed(Keys key)
+        {
+            return curState.IsKeyDown(key) && !lastState.IsKeyDown(key);
         }
     }
 }
